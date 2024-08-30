aeaf25897bb8861bc15d23fb88b2c4d19c8fb35d
devdeepakmane <devdeepakmane@gmail.com>
2024-08-30 17:32:51 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors";
-import { connectDB } from "./data/database.js";
 import mongoose from "mongoose";
-const PORT = process.env.port || 5000;
-config({
-  path: "./data/config.env",
-});
+
+dotenv.config();
+
 const app = express();
 // Using Middlewares
+const PORT = process.env.port;
+console.log(PORT);
 
 // app.use(
 //   cors({
